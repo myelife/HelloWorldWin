bd000889ad6fd2a6c44227a80c29752e1bab7c56
Mike <fe.dimitrisk@gmail.com>
2023-04-08 00:09:31 +0900

    Added toggleButton

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,9 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+
+    addAndMakeVisible(toggleButton);
+
     setSize (600, 400);
 }
 
@@ -16,7 +19,7 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (48.0f));
+    g.setFont (juce::Font (44.0f));
     g.setColour (juce::Colours::red);
     g.drawText ("Hello New PFM::C++ Master", getLocalBounds(), juce::Justification::centred, true);
 }
@@ -26,4 +29,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    toggleButton.setBounds(10, 10, 30, 30);
 }
